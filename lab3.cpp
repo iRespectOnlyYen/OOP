61bd8bccd69d71b945093c0e2d042f7710f902e1
iRespectOnlyYen <90966720+iRespectOnlyYen@users.noreply.github.com>
2022-04-12 23:45:44 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 #include <conio.h>
 #include <iomanip>
@@ -21,36 +21,29 @@ void number1()
 
     }
 }
+
+double power1(double n, int p = 2)
+{
+    return pow(n, p);
+}
 void number2()
 {
-    int response;
-    double temper;
-
-
-
-    cout << "\nType 1 to convert fahrenheit to celsius," << "\n2 to convert celsius to fahrenheit : ";    cin >> response;
-
-    if (response == 1)
-
+    double number, answer;
+    int pow;
+    char yeserno;
+    cout << "\nВведите число:"; // get number
+    cin >> number;
+    cout << "Будете вводить степень (y/n)?";
+    cin >> yeserno;
+    if (yeserno == 'y')// пользователю нужен не квадрат числа?
     {
-
-        cout << "Enter temperature in fahrenheit: ";
-        cin >> temper;
-        cout << "In celsius that's " << 5.0 / 9.0 * (temper - 32.0);
-
+        cout << "Введите степень:";
+        cin >> pow;
+        answer = power1(number, pow); // возвести число в степень
     }
     else
-
-    {
-
-        cout << "Enter temperature in celsius: ";
-        cin >> temper;
-        cout << "In fahrenheit that's " << 9.0 / 5.0 * temper + 32.0;
-
-    }
-
-    cout << endl;
-
+        answer = power1(number); // квадрат числа
+    cout << "Ответ " << answer << endl;
 }
 
 void number3()
@@ -94,394 +87,62 @@ void number4()
 
 }
 
+
+int hms_to_secs(int hours, int minutes, int seconds) {
+    int32_t all_seconds = seconds + minutes * 60 + hours * 60 * 60;
+    return all_seconds;
+}
 void number5()
 {
-    struct data
-    {
-        int mounth;
-        int day;
-        int years;
-    };
+    int x = 0;
+    while (x == 0) {
 
-    data user;
+        int hours;
+        int minutes;
+        int seconds;
+        char separator_character;
 
-    cout << "Введите дату: ";
-    cin >> user.mounth;
-    char slash;
-    cin >> slash;
-    cin >> user.day;
-    cin >> slash;
-    cin >> user.years;
-    cout << "Дата: " << user.mounth << slash << user.day << slash << user.years;
+        cout << "Введите часы, минуты и секунды (например 20:08:30): ";
+        cin >> hours >> separator_character >> minutes >> separator_character >> seconds;
+
+        cout << "Время в секундах: " << hms_to_secs(hours, minutes, seconds) << endl;
+
+        cout << "Желаете продолжить? (y/n): ";
+        char continue_programm;
+        cin >> continue_programm;
+        if (continue_programm == 'y') {}
+        else if (continue_programm == 'n') { break; }
+        else cout << "Неверный символ";
+        cout << endl;
+    }
 }
 
+struct time1
+{
+    int hour;
+    int minutes;
+    int seconds;
+};
+int time_to_secs(int hours, int minutes, int seconds) {
+    int32_t all_seconds = seconds + minutes * 60 + hours * 60 * 60;
+    return all_seconds;
+}
+time1 secs_to_time(int seconds) {
+
+
+
+    time1 t;
+
+    t.seconds = seconds % 60;
+    seconds /= 60;
+    t.minutes = seconds % 60;
+    t.hour = seconds / 60;
+    return (t);
+}
 void number6()
 {
 
-    enum etype { laborer, secretary, manager, accountant, executive, researcher };
-
-
-    cout << "Введите букву: ";
-    char x;
-    cin >> x;
-    etype position;
-    switch (x)
-    {
-    case 'l':
-        position = laborer;
-        break;
-    case 's':
-        position = secretary;
-        break;
-    case 'm':
-        position = manager;
-        break;
-    case  'a':
-        position = accountant;
-        break;
-    case 'e':
-        position = executive;
-        break;
-    case 'r':
-        position = researcher;
-        break;
-    }
-
-    switch (position)
-    {
-    case 0:
-        cout << "laborer";
-        break;
-    case 1:
-        cout << "secretary";
-        break;
-    case 2:
-        cout << "manager";
-        break;
-    case 3:
-        cout << "accountant";
-        break;
-    case 4:
-        cout << "executive";
-        break;
-    case 5:
-        cout << "researcher";
-        break;
-    }
-
-}
-
-void number7()
-{
-
-
-    enum etype { laborer, secretary, manager, accountant, executive, researcher };
-
-    struct Data
-    {
-        int mounth;
-        int day;
-        int years;
-    };
-
-    struct Employee
-    {
-        int number;
-        float money;
-        Data start_work;
-        etype position;
-    };
-
-    char slash;
-
-    Employee worker_info1, worker_info2, worker_info3;
-
-    cout << "Введите номер первого сотрудника: ";
-    cin >> worker_info1.number;
-
-    cout << "Введите пособие первого сотрудника: ";
-    cin >> worker_info1.money;
-
-    cout << "Введите дату устройства на работу первого сотрудника: ";
-    cin >> worker_info1.start_work.mounth;
-    cin >> slash;
-    cin >> worker_info1.start_work.day;
-    cin >> slash;
-    cin >> worker_info1.start_work.years;
-
-    
-
-    cout << "Введите первую букву профессии: ";
-    char x;
-    cin >> x;
-    switch (x)
-    {
-    case 'l':
-        worker_info1.position = laborer;
-        break;
-    case 's':
-        worker_info1.position = secretary;
-        break;
-    case 'm':
-        worker_info1.position = manager;
-        break;
-    case  'a':
-        worker_info1.position = accountant;
-        break;
-    case 'e':
-        worker_info1.position = executive;
-        break;
-    case 'r':
-        worker_info1.position = researcher;
-        break;
-    }
-
-   
-
-    cout << endl << endl;
-
-
-
-
-    cout << "Введите номер второго сотрудника: ";
-    cin >> worker_info2.number;
-
-    cout << "Введите пособие второго сотрудника: ";
-    cin >> worker_info2.money;
-
-    cout << "Введите дату устройства на работу второго сотрудника: ";
-    cin >> worker_info2.start_work.mounth;
-    cin >> slash;
-    cin >> worker_info2.start_work.day;
-    cin >> slash;
-    cin >> worker_info2.start_work.years;
-
-
-    cout << "Введите второго букву профессии: ";
-    
-    cin >> x;
-    switch (x)
-    {
-    case 'l':
-        worker_info2.position = laborer;
-        break;
-    case 's':
-        worker_info2.position = secretary;
-        break;
-    case 'm':
-        worker_info2.position = manager;
-        break;
-    case  'a':
-        worker_info2.position = accountant;
-        break;
-    case 'e':
-        worker_info2.position = executive;
-        break;
-    case 'r':
-        worker_info2.position = researcher;
-        break;
-    }
-
-    
-    cout << endl << endl;
-
-
-    cout << "Введите номер третьего сотрудника: ";
-    cin >> worker_info3.number;
-
-    cout << "Введите пособие третьего сотрудника: ";
-    cin >> worker_info3.money;
-
-    cout << "Введите дату устройства на работу третьего сотрудника: ";
-    cin >> worker_info3.start_work.mounth;
-    cin >> slash;
-    cin >> worker_info3.start_work.day;
-    cin >> slash;
-    cin >> worker_info3.start_work.years;
-
-    cout << "Введите третьего букву профессии: ";
- 
-    cin >> x;
-    switch (x)
-    {
-    case 'l':
-        worker_info3.position = laborer;
-        break;
-    case 's':
-        worker_info3.position = secretary;
-        break;
-    case 'm':
-        worker_info3.position = manager;
-        break;
-    case  'a':
-        worker_info3.position = accountant;
-        break;
-    case 'e':
-        worker_info3.position = executive;
-        break;
-    case 'r':
-        worker_info3.position = researcher;
-        break;
-    }
-
-    cout << endl << endl;  
-
-    cout << "Первый сотрудник" << endl << "Зарплата: " << worker_info1.money << endl << "Номер: " << worker_info1.number << endl << "Дата устройства: " << worker_info1.start_work.mounth << slash << worker_info1.start_work.day << slash << worker_info1.start_work.years << endl << "Должность: ";
-    switch (worker_info1.position)
-    {
-    case laborer:
-        cout << "laborer";
-        break;
-    case secretary:
-        cout << "secretary";
-        break;
-    case manager:
-        cout << "manager";
-        break;
-    case accountant:
-        cout << "accountant";
-        break;
-    case executive:
-        cout << "executive";
-        break;
-    case researcher:
-        cout << "researcher";
-        break;
-    }
-
-    cout << endl << endl;
-
-    cout << "Второе сотрудник" << endl << "Зарплата: " << worker_info2.money << endl << "Номер: " << worker_info2.number << endl << "Дата устройства: " << worker_info2.start_work.mounth << slash << worker_info2.start_work.day << slash << worker_info2.start_work.years << endl << "Должность: ";
-    switch (worker_info2.position)
-    {
-    case laborer:
-        cout << "laborer";
-        break;
-    case secretary:
-        cout << "secretary";
-        break;
-    case manager:
-        cout << "manager";
-        break;
-    case accountant:
-        cout << "accountant";
-        break;
-    case executive:
-        cout << "executive";
-        break;
-    case researcher:
-        cout << "researcher";
-        break;
-    }
-
-    cout << endl << endl;
-    cout << "Третий сотрудник" << endl << "Зарплата: " << worker_info3.money << endl << "Номер: " << worker_info3.number << endl << "Дата устройства: " << worker_info3.start_work.mounth << slash << worker_info3.start_work.day << slash << worker_info3.start_work.years << endl << "Должность: ";
-    switch (worker_info3.position)
-    {
-    case laborer:
-        cout << "laborer";
-        break;
-    case secretary:
-        cout << "secretary";
-        break;
-    case manager:
-        cout << "manager";
-        break;
-    case accountant:
-        cout << "accountant";
-        break;
-    case executive:
-        cout << "executive";
-        break;
-    case researcher:
-        cout << "researcher";
-        break;
-    }
-
-}
-
-void number8()
-{
-    cout << "Программа складывает дроби находя общий знаминатель" << endl << endl;
-    struct fraction
-    {
-        int chisl;
-        int znam;
-    };
-    fraction drob1, drob2, finale;
-    char slash;
-
-    cout << "Введите первую дробь: ";
-    cin >> drob1.chisl >> slash >> drob1.znam;
-
-    cout << "Введите вторую дробь: ";
-    cin >> drob2.chisl >> slash >> drob2.znam;
-
-    finale.chisl = (drob1.chisl * drob2.znam) + (drob2.chisl * drob1.znam);
-    finale.znam = drob1.znam * drob2.znam;
-
-    cout << "\nСумма равна: " << finale.chisl << '/' << finale.znam;
-
-    double a = finale.chisl;
-    double b = finale.znam;
-
-    while (a != b) {
-        if (a > b) a = a - b;
-        else b = b - a;
-    }
-    if (a > 1) cout << "\nМожно скоратить до: " << finale.chisl / a << '/' << finale.znam / a;
-}
-
-void number9()
-{
-    struct time
-    {
-        int hour;
-        int minutes;
-        int seconds;
-    };
-    time t1;
-    char slash;
-    cout << "Введите время (например 12:36:56): ";
-    cin >> t1.hour >> slash >> t1.minutes >> slash >> t1.seconds;
-    long totalsecs = t1.hour * 3600 + t1.minutes * 60 + t1.seconds;
-    cout << "totalsecs = " << totalsecs;
-}
-
-void number10()
-{
-    struct sterling
-    {
-        int pounds;
-        int shillings;
-        int pence;
-    };
-
-    sterling old;
-    double funt;
-
-    cout << "Программа конвертирует сумму из новой Британской денежной системы в старую Британскую денежную систему." << endl << endl;
-    cout << "Введите количество фунтов: ";
-    cin >> funt;
-
-    int int_funt = funt;
-    old.shillings = (funt - int_funt) * 20.;
-    int int_old_shill = old.shillings;
-    old.pence = (old.shillings - int_old_shill) * 12.;
-
-    cout << "В старой Британской денежной системе: ";
-    cout << int_funt << '.' << int_old_shill << '.' << (int)old.pence;
-}
-
-void number11()
-{
-    struct time
-    {
-        int hour;
-        int minutes;
-        int seconds;
-    };
-    time t1,t2,t_finale;
+    time1 t1, t2, t_finale;
     char slash;
     cout << "Введите время1 (например 12:36:56): ";
     cin >> t1.hour >> slash >> t1.minutes >> slash >> t1.seconds;
@@ -489,25 +150,185 @@ void number11()
     cout << "Введите время2 (например 12:36:56): ";
     cin >> t2.hour >> slash >> t2.minutes >> slash >> t2.seconds;
 
-    long totalsecs1 = t1.hour * 3600 + t1.minutes * 60 + t1.seconds;
-    long totalsecs2 = t2.hour * 3600 + t2.minutes * 60 + t2.seconds;
-    long totalsecs_finale = totalsecs1 + totalsecs2;
-    t_finale.seconds = totalsecs_finale % 60;
-    totalsecs_finale /= 60;
-    t_finale.minutes = totalsecs_finale % 60;
-    t_finale.hour = totalsecs_finale / 60;
+    int64_t all_seconds = time_to_secs(t1.hour, t1.minutes, t1.seconds) + time_to_secs(t2.hour, t2.minutes, t2.seconds);
+    t_finale = secs_to_time(all_seconds);
     cout << "Сумма времени: " << t_finale.hour << slash << t_finale.minutes << slash << t_finale.seconds;
+
 }
 
+double power1(int n, int p = 2)
+{
+    return pow(n, p);
+}
+double power1(long n, int p = 2)
+{
+    return pow(n, p);
+}
+double power1(float n, int p = 2)
+{
+    return pow(n, p);
+}
+double power1(char n, int p = 2)
+{
+    return pow(n, p);
+}
+void number7()
+{
+    cout << "Введите аргументы типов: double, int, long, float, char." << endl;
+    double a;
+    int b;
+    long c;
+    float d;
+    char e;
+    cin >> a >> b >> c >> d >> e;
+    cout << power1(a)<<endl;
+    cout << power1(b)<< endl;
+    cout << power1(c) << endl;
+    cout << power1(d) << endl;
+    cout << power1(e) << endl;
+}
+
+
+void swap1(int& a, int& b) 
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void number8()
+{
+    cout << "Введите значения переменных A и B, программа поменяет их значения местами. " << endl;
+    int a, b;
+    cin >> a >> b;
+    swap1(a, b);
+    cout << a <<' '<< b;
+}
+
+
+void swap_time(time1& a, time1& b) 
+{
+    time1 temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void number9()
+{
+    cout << "Введите значения переменных A и B, программа поменяет их значения местами. " << endl;
+    time1 a, b;
+    char slash;
+    cin >> a.hour >> slash >> a.minutes >> slash >> a.seconds;
+    cin >> b.hour >> slash >> b.minutes >> slash >> b.seconds;
+    swap_time(a,b);
+    cout << endl<< a.hour << slash << a.minutes << slash << a.seconds;
+    cout << endl<<b.hour << slash << b.minutes << slash << b.seconds;
+}
+
+int count_test = 0;
+void func_global() 
+{
+    count_test++;
+    cout <<"Функция была вызванна "<<count_test<<" раз(а)"<<endl;
+}
+void func_static()
+{
+    static int counter_static = 0;
+    counter_static++;
+    cout << "Функция была вызванна " << counter_static << " раз(а)" << endl;
+}
+void number10()
+{
+    
+    for (int i = 0;i < 10; i++) 
+    {
+        func_global();
+    }
+    cout << endl;
+
+    for (int i = 0;i < 10; i++)
+    {
+        func_static();
+    }
+}
+
+
+struct sterling
+{
+    int pounds;
+    int shillings;
+    int pence;
+};
+void print_sterling(sterling sterling) 
+{
+    cout << "Сумма стерлингов: ";
+    cout << sterling.pounds <<'.'<< sterling.shillings << '.' << sterling.pence;
+}
+sterling sum_sterling(sterling value1, sterling value2) 
+{
+    sterling sum_sterling;
+    sum_sterling.pounds = value1.pounds + value2.pounds;
+    sum_sterling.shillings = value1.shillings + value2.shillings;
+    sum_sterling.pence = value1.pence + value2.pence;
+    sum_sterling.shillings = sum_sterling.pence / 12 + sum_sterling.shillings;
+    sum_sterling.pence = sum_sterling.pence % 12;
+    sum_sterling.pounds = sum_sterling.pounds + sum_sterling.shillings / 20;
+    sum_sterling.shillings = sum_sterling.shillings % 20;
+    return sum_sterling;
+
+}
+sterling get_sterling(sterling& value) {
+    cout << "Введите кол-во стерлингов (пример: 1.1.1)"<<endl;
+    char slash;
+    cin >> value.pounds >>slash>> value.shillings >>slash>> value.pence;
+    return value;
+}
+void number11()
+{
+    sterling value1, value2;
+    get_sterling(value1);
+    get_sterling(value2);
+    print_sterling(sum_sterling(value1, value2));
+
+}
+
+
+struct fraction
+{
+    int chisl;
+    int znam;
+};
+fraction f_addition(fraction d1, fraction d2)
+{
+    fraction d_totale;
+    d_totale.chisl = (d1.chisl * d2.znam + d1.znam * d2.chisl);
+    d_totale.znam = (d1.znam * d2.znam);
+    return d_totale;
+}
+fraction f_subtraction(fraction d1, fraction d2)
+{
+    fraction d_totale;
+    d_totale.chisl = (d1.chisl * d2.znam - d1.znam * d2.chisl);
+    d_totale.znam = (d1.znam * d2.znam);
+    return d_totale;
+}
+fraction f_multiplication(fraction d1, fraction d2)
+{
+    fraction d_totale;
+    d_totale.chisl = (d1.chisl * d2.chisl); 
+    d_totale.znam = (d1.znam * d2.znam);
+    return d_totale;
+}
+fraction f_division(fraction d1, fraction d2)
+{
+    fraction d_totale;
+    d_totale.chisl = (d1.chisl * d2.znam); 
+    d_totale.znam = (d1.znam * d2.chisl);
+    return d_totale;
+}
 void number12()
 {
-    struct fraction
-    {
-        int chisl;
-        int znam;
-    };
-
-    fraction d1, d2,d_totale;
+    fraction d1, d2, d_totale;
 
     cout << "Программа выполняет четыре арифемтических действия над дробями\n(Пример: 12/2 + 12/2)" << endl << endl;
     char slash, deistvie, next_or_stop = 'y';
@@ -520,13 +341,13 @@ void number12()
         switch (deistvie)
 
         {
-        case '+':  d_totale.chisl = (d1.chisl * d2.znam + d1.znam * d2.chisl); d_totale.znam = (d1.znam * d2.znam);
+        case '+':  d_totale = f_addition(d1, d2);
             break;
-        case '-':  d_totale.chisl = (d1.chisl * d2.znam - d1.znam * d2.chisl); d_totale.znam = (d1.znam * d2.znam);
+        case '-':  d_totale = f_subtraction(d1, d2);
             break;
-        case '*':  d_totale.chisl = (d1.chisl * d2.chisl); d_totale.znam = (d1.znam * d2.znam);
+        case '*':  d_totale = f_multiplication(d1, d2);
             break;
-        case '/':  d_totale.chisl = (d1.chisl * d2.znam); d_totale.znam = (d1.znam * d2.chisl);
+        case '/':  d_totale = f_division(d1, d2);
             break;
 
         }
